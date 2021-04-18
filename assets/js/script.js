32d0f9f6a18d24f6e4a0934525f08856c7f91f52
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-04-18 12:55:15 -0700

    defined getWeather and renderWeather functions within script file. modified index and style files to get getWeather and renderWeather functions to render properly.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,66 @@
+// define global variables
+var searchHistory = [];
+const apiKey = "a57dbe6e264ba286bdc558c04ad72f43";
+
+
 // submit button pulls weather and forecast of the city and adds it to the search history
-var submitBtnEl = document.querySelector("#search-button");
-submitBtnEl.addEventListener("click", searchHandler);
+var submitBtnEl = $("#search-button");
+submitBtnEl.on("click", searchHandler);
 
 function searchHandler(event) {
   event.preventDefault();
 
   var searchInputEl = document.querySelector("#search-city");
   var searchCity = searchInputEl.value.trim();
+
+  if(searchCity) {
+    getWeather(searchCity);
+  } else {
+    alert("Field cannot be left blank");
+  }
 }
 
 // get weather
-function getWeather() {
-  var apiKey = "a57dbe6e264ba286bdc558c04ad72f43";
-  var apiUrl = `http://api.openweathermap.org/data/2.5/weater?q=${searchEl}&appid=${apiKey}`;
+function getWeather(searchCity) {
+
+  var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=imperial&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+        renderWeather(data, searchCity);
+      });
+    });
 }
 
 // render weather
-function renderWeather() {
+function renderWeather(currentWeather, searchCity) {
+  
+  // render each element within the weather container
+  var weatherEl = $("#weather-container");
+  weatherEl.addClass("show");
+  
+  var cityNameEl = $("#city-name");
+  cityNameEl.text(searchCity);
+  
+  var currentDateEl = $("#current-date");
+  var currentDate = moment().format("M/D/YYYY");
+  currentDateEl.text(`(${currentDate})`);
 
+  var currentIconEl = $("#current-icon");
+  currentIconEl.attr("src", `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}.png`)
+  
+  var cityTempEl = $("#city-temperature");
+  cityTempEl.text(currentWeather.main.temp);
+
+  var cityHumidityEl = $("#city-humidity");
+  cityHumidityEl.text(currentWeather.main.humidity);
+
+  var cityWindEl = $("#city-wind-speed");
+  cityWindEl.text(currentWeather.wind.speed);
+
+  // var cityUvEl = $("#city-uv-index");
 }
 
 // get forecast
@@ -31,14 +74,19 @@ function renderForecast() {
 }
 
 // save search to history
-function saveSearch() {
-
+function saveSearch(searchCity) {
+  searchHistory.push(searchCity);
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 }
+
+// render search history
+
+
 
 // city selected from search history pulls current weather and forecast of the city
-var historyBtnEl = document.querySelector("#search-history");
-historyBtnEl.addEventListener("click", historyHandler);
+// var historyBtnEl = $("#search-history");
+// historyBtnEl.aon("click", historyHandler);
 
-function historyHandler(event) {
-  event.preventDefault();
-}
+// function historyHandler(event) {
+//   event.preventDefault();
+// }
