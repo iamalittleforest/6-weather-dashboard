06f044b67afb1aaf423789b4fc0b42816c34613c
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-04-18 22:01:28 -0700

    defined fetchForecast, renderForecast, and saveSearch functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ function searchHandler(event) {
   event.preventDefault();
 
   var searchInputEl = document.querySelector("#search-city");
-  var searchCity = searchInputEl.value.trim();
+  var searchCity = searchInputEl.value.toLowerCase().trim();
 
   if(searchCity) {
     fetchWeather(searchCity);
@@ -122,43 +122,100 @@ function fetchForecast(cityLat, cityLon) {
 // render forecast
 function renderForecast(forecast) {
   
-  // var forecast = forecastWeather.list;
+  // render day 1 forecast
+  var day1El = $("#day1");
+  day1El.text(moment.unix(forecast.daily[1].dt).format("M/D/YYYY"));
 
-  // for (var i=0; forecast.length; i++) {
-
-    
-  // var forecastEl = $(".forecast");
-
-  // var currentDateEl = $("#current-date");
-  // var currentDate = moment().format("M/D/YYYY");
-  // currentDateEl.text(forecast.daily[0].dt);
-
-  // var currentIconEl = $("#current-icon");
-  // currentIconEl.attr("src", `https://openweathermap.org/img/wn/${forecast.daily[0].weather[0].icon}.png`)
+  var day1IconEl = $("#day1-icon");
+  day1IconEl.attr("src", `https://openweathermap.org/img/wn/${forecast.daily[1].weather[0].icon}.png`)
   
-  // var cityTempEl = $("#city-temperature");
-  // cityTempEl.text(forecast.daily[0].temp.day);
+  var day1TempEl = $("#day1-temperature");
+  day1TempEl.text(forecast.daily[1].temp.day);
 
-  // var cityHumidityEl = $("#city-humidity");
-  // cityHumidityEl.text(forecast.daily[0].humidity);
-  // }
+  var day1HumidityEl = $("#day1-humidity");
+  day1HumidityEl.text(forecast.daily[1].humidity);
+
+  // render day 2 forecast
+  var day2El = $("#day2");
+  day2El.text(moment.unix(forecast.daily[2].dt).format("M/D/YYYY"));
+
+  var day2IconEl = $("#day2-icon");
+  day2IconEl.attr("src", `https://openweathermap.org/img/wn/${forecast.daily[2].weather[0].icon}.png`)
+  
+  var day2TempEl = $("#day2-temperature");
+  day2TempEl.text(forecast.daily[2].temp.day);
+
+  var day2HumidityEl = $("#day2-humidity");
+  day2HumidityEl.text(forecast.daily[2].humidity);
+
+  // render day 3 forecast
+  var day3El = $("#day3");
+  day3El.text(moment.unix(forecast.daily[3].dt).format("M/D/YYYY"));
+
+  var day3IconEl = $("#day3-icon");
+  day3IconEl.attr("src", `https://openweathermap.org/img/wn/${forecast.daily[3].weather[0].icon}.png`)
+  
+  var day3TempEl = $("#day3-temperature");
+  day3TempEl.text(forecast.daily[3].temp.day);
+
+  var day3HumidityEl = $("#day3-humidity");
+  day3HumidityEl.text(forecast.daily[3].humidity);
+
+  // render day 4 forecast
+  var day4El = $("#day4");
+  day4El.text(moment.unix(forecast.daily[4].dt).format("M/D/YYYY"));
+
+  var day4IconEl = $("#day4-icon");
+  day4IconEl.attr("src", `https://openweathermap.org/img/wn/${forecast.daily[4].weather[0].icon}.png`)
+  
+  var day4TempEl = $("#day4-temperature");
+  day4TempEl.text(forecast.daily[4].temp.day);
+
+  var day4HumidityEl = $("#day4-humidity");
+  day4HumidityEl.text(forecast.daily[4].humidity);
+
+  // render day 5 forecast
+  var day5El = $("#day5");
+  day5El.text(moment.unix(forecast.daily[5].dt).format("M/D/YYYY"));
+
+  var day5IconEl = $("#day5-icon");
+  day5IconEl.attr("src", `https://openweathermap.org/img/wn/${forecast.daily[5].weather[0].icon}.png`)
+  
+  var day5TempEl = $("#day5-temperature");
+  day5TempEl.text(forecast.daily[5].temp.day);
+
+  var day5HumidityEl = $("#day5-humidity");
+  day5HumidityEl.text(forecast.daily[5].humidity);
 }
 
-// save search to history
+// save search
 function saveSearch(searchCity) {
+  
+  // add city to search history
   searchHistory.push(searchCity);
   localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-  renderSearch();
+
+  // create button
+  var cityBtn = $("<button>");
+  cityBtn.text(`${searchCity}`);
+  cityBtn.attr("type", "button");
+  cityBtn.addClass("list-group-item list-group-item-action");
+
+  // add button to search history
+  var searchList = $(".search-history")
+  searchList.prepend(cityBtn);
 }
 
 // render search history
 function renderSearch() {
 
   // var savedCities = JSON.parse(localStorage.getItem("searchHistory"));
+  // console.log(savedCities);
   
   // // checks to see if data is stored in localStorage
   // if (savedCities !== null) {
   //     searchHistory = savedCities;
+  //     console.log(searchHistory);
   // } else {
   //     searchHistory = [];
   // }
@@ -168,7 +225,6 @@ function renderSearch() {
   // for (var i = 0; i < searchHistory.length; i++) {
   //   var cityBtn = $("<button>");
   //   cityBtn.text = `${searchHistory[i]}`;
-  //   cityBtn.attr("data-index", i);
   //   cityBtn.addClass("list-group-item", "list-group-item-action");
   //   searchList.append(cityBtn);
   // }  
